bf801f4c65ab85d2edd20efd9210a924145cc0aa
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2022-10-31 17:50:37 -0500

    Delete takemehome directory

--- takemehome/src/main/java/pe/edu/upc/takemehome/repositories/ShipmentRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.takemehome.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.takemehome.entities.Shipment;
-//import pe.edu.upc.takemehome.entities.Order;
-
-public interface ShipmentRepository extends JpaRepository<Shipment,Long>{
-    //Shipment FindByOrder(Order order);
-}
