c78494d187112bfd19f8aa2d215264b92cfc46cc
Omar V <64499782+OmarV888@users.noreply.github.com>
2022-11-14 18:15:55 -0500

    Delete Backend_TakemeHome directory

--- Backend_TakemeHome/src/main/java/pe/edu/upc/takemehome/entities/State.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.takemehome.entities;
-
-public enum State {
-    Reserved,
-    Shipping,
-    Delivered,
-    Canceled
-}
--- Backend_TakemeHome/src/main/java/pe/edu/upc/takemehome/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.takemehome.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.takemehome.entities.Order;
-
-public interface OrderRepository  extends JpaRepository<Order,Long> {
-}
